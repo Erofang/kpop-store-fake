cc00d4f52f2b818826a172e665bf2d9dd77495b0
Dedline <matous.kader@seznam.cz>
2023-01-13 21:20:43 +0100

    lalalal

--- controllers/register.js
+++ controllers/register.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const bcrypt = require('bcrypt');
+const User = require('../models/User');
 const router = express.Router();
 
 
@@ -7,6 +9,19 @@ router.get('/', (req, res) => {
         title: 'Register',
         style: 'register.css'
     })
+})
+
+
+router.post('/', async (req, res) => {
+    try {
+        const {name, surname, tel, email, password} = req.body;
+        console.log(req.body);
+        const hashedPassword = await bcrypt.hash(password, 10);
+        User.register(name, surname, tel, email, hashedPassword);
+        res.redirect('/login');
+    } catch  {
+        res.redirect('/');
+    }
 })
 
 
